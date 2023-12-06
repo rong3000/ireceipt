1cec10c27a8b95868aba7c08fd61a6f9b3b0d6d2
Rong <rong.lin3000@gmail.com>
2023-12-06 17:35:01 +1100

    Appbar and bottom tab working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DrawerMenu from '../DrawerMenu';
 import Home from '../Home';
 import Inbox from '../Inbox';
 import Drafts from '../Drafts';
+
+import { AppBar, Toolbar, Typography, ListItemIcon } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import BottomTabs from '../BottomTabs';
 
 const App = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -14,16 +18,26 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={toggleDrawer}>Open Drawer</button>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            <ListItemIcon>
+              <MenuIcon onClick={toggleDrawer}/>
+            </ListItemIcon>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <DrawerMenu open={drawerOpen} onClose={toggleDrawer} />
       
-        <DrawerMenu open={drawerOpen} onClose={toggleDrawer} />
-        
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/inbox" element={<Inbox />} />
-          <Route path="/drafts" element={<Drafts />} />
-        </Routes>
-      
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/drafts" element={<Drafts />} />
+      </Routes>
+
+      <BottomTabs/>
+
     </div>
   );
 };
