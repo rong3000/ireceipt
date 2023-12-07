a80c66e03c310b7b7002907256d02f6a19613d20
rong3000 <rong.lin3000@gmail.com>
2023-12-07 17:43:16 +1100

    use rtk Query for http get

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,20 @@
 import { connect } from 'react-redux'
 
+import React, { useEffect, useState, useRef } from 'react';
+
 import { login, logout } from '../../redux/actions/authActions';
+
+import { useGetReceiptsQuery } from '../../datamodel/rtkQuerySlice';
 
 const Login = ({ isLoggedIn, user, count, login, logout }) => {
 
+  const { data, error, isLoading, refetch } = useGetReceiptsQuery(
+    {
+      pollingInterval: 3000,
+      refetchOnMountOrArgChange: true,
+      skip: false,
+    }
+  );
   const handleLogin = () => {
     const USERNAME = process.env.REACT_APP_USERNAME
     const PASSWORD = process.env.REACT_APP_PASSWORD
@@ -15,6 +26,10 @@ const Login = ({ isLoggedIn, user, count, login, logout }) => {
     logout();
   };
 
+  const getUserReceipts = () => {
+    refetch();
+  }
+
   return (
     <div>
       {isLoggedIn ? (
@@ -24,6 +39,20 @@ const Login = ({ isLoggedIn, user, count, login, logout }) => {
           <h1>In Component above Current Sum is: {count}</h1>
           <h2>Token is, {user.accessToken}!</h2>
           <button onClick={handleLogout}>Logout</button>
+          <button onClick={getUserReceipts}>getUserReceipts</button>
+          <div>
+            {isLoading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>Error: {error.message}</div>
+            ) : data ? (
+              <div>
+                <h2>Welcome, {data?.messageCode}!</h2>
+                <h2>Hi, {data?.isSuccess.toString()}!</h2>
+                <h2>companyName, {data?.obj[0]?.companyName}!</h2>
+              </div>
+            ) : null}
+          </div>
         </>
       ) : (
         <>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query/react'
+import { api } from '../datamodel/rtkQuerySlice'
 
 import authReducer from './reducers/authReducer';
 import countReducer from './reducers/countReducers'
@@ -7,6 +9,14 @@ const store = configureStore({
   reducer: {
     authx: authReducer,
     count: countReducer,
-  }
-})
+    [api.reducerPath]: api.reducer,
+
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch)
+
 export default store;
