17c9a5bbe09be48438063b53a210c30e27a724d2
Rong <rong.lin3000@gmail.com>
2023-12-08 11:01:51 +1100

    can display images

--- src/components/Receipts/index.jsx
+++ src/components/Receipts/index.jsx
@@ -1,7 +1,108 @@
-import React from 'react';
+import { connect } from 'react-redux'
 
-const Receipts = () => {
-  return <h2>Receipts Page</h2>;
+import { useGetReceiptsQuery } from '../../datamodel/rtkQuerySlice';
+
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+import {
+  Avatar,
+  Card,
+  Container,
+  Rating,
+  Tooltip,
+} from '@mui/material';
+import { StarBorder } from '@mui/icons-material';
+
+
+const Receipts = ({ isLoggedIn }) => {
+
+  const { data, error, isLoading, refetch, isUninitialized } = useGetReceiptsQuery(
+    undefined,
+    {
+      skip: !isLoggedIn
+    }
+  );
+
+  if (!isLoggedIn) {
+    return <div>Not logged in yet and {isUninitialized.toString()}</div>
+  }
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <>
+          <div>
+            {isLoading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>Error: {error.message}</div>
+            ) : data ? (
+              <Container>
+                <ImageList
+                  gap={12}
+                  sx={{
+                    mb: 8,
+                    gridTemplateColumns:
+                      'repeat(auto-fill, minmax(280px, 1fr))!important',
+                  }}
+                >
+                  {data.obj.map((item) => (
+                    <Card key={item.id}>
+                      <ImageListItem sx={{ height: '100% !important' }}>
+                        <ImageListItemBar
+                          sx={{
+                            background:
+                              'linear-gradient(to bottom, rgba(0,0,0,0.7)0%, rgba(0,0,0,0.3)70%, rgba(0,0,0,0)100%)',
+                          }}
+                          title={item.totalAmount === 0 ? 'Free Stay' : '$' + item.totalAmount}
+                          actionIcon={
+                            <Tooltip title={item.companyName} sx={{ mr: '5px' }}>
+                              <Avatar src={`https://api.ireceipts.au/Receipt/GetImage/${encodeURIComponent(item.imagePath)}`} />
+                            </Tooltip>
+                          }
+                          position="top"
+                        />
+                        <img
+                          src={`https://api.ireceipts.au/Receipt/GetImage/${encodeURIComponent(item.imagePath)}`}
+                          alt={item.companyName}
+                          loading="lazy"
+                          style={{ cursor: 'pointer' }}
+                        />
+                        <ImageListItemBar
+                          title={item.companyName}
+                          actionIcon={
+                            <Rating
+                              sx={{ color: 'rgba(255,255,255, 0.8)', mr: '5px' }}
+                              name="item-rating"
+                              defaultValue={3.5}
+                              precision={0.5}
+                              emptyIcon={
+                                <StarBorder sx={{ color: 'rgba(255,255,255, 0.8)' }} />
+                              }
+                            />
+                          }
+                        />
+                      </ImageListItem>
+                    </Card>
+                  ))}
+                </ImageList>
+              </Container>
+            ) : null}
+          </div>
+        </>
+      ) : (
+        <>
+        </>
+      )}
+    </div>
+  )
 };
 
-export default Receipts;
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.authx.isLoggedIn
+});
+
+export default connect(mapStateToProps, {})(Receipts);
+
