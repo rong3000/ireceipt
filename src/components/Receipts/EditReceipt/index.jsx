b1cf6a693796457582cfdaf57e964873682c00b1
Rong <rong.lin3000@gmail.com>
2023-12-11 12:02:05 +1100

    can actually edit receipt

--- src/components/Receipts/EditReceipt/index.jsx
+++ src/components/Receipts/EditReceipt/index.jsx
@@ -8,7 +8,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import {
-  Box,
+Box,
   OutlinedInput,
   InputLabel,
   InputAdornment,
@@ -18,11 +18,52 @@ import {
   ImageListItem,
   Button
 } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useUpdateReceiptMutation } from '../../../datamodel/rtkQuerySlice';
 
 export default function EditReceipt() {
 
+  const [updateReceipt, { data, error, isLoading, isSuccess, isError }] = useUpdateReceiptMutation();
+
+  const navigate = useNavigate();
+
   const location = useLocation();
   const item = location.state?.item;
+
+  const handleConfirmClick = () => {
+    console.log(`date is ${date} and receiptDate is ${dayjs(item?.receiptDatetime)}`);
+    console.log((date.isSame(dayjs(item?.receiptDatetime))));
+    console.log(`amount is ${amount} and receiptAmount is ${item?.totalAmount}`);
+    console.log((amount === item?.totalAmount));
+    console.log(`name is ${name} and receiptCompanyName is ${item?.companyName}`);
+    console.log((name === item?.companyName));
+    let modifiedData = {
+      receiptDatetime: date.format('YYYY-MM-DD[T]hh:mm:ss'),
+      totalAmount: amount * 1,
+      companyName: name
+    }
+    let receiptData = {
+      ...item,
+      ...modifiedData
+    };
+    console.log(receiptData);
+
+    if ((date.isSame(dayjs(item?.receiptDatetime))) && amount === item?.totalAmount && name === item?.companyName) {
+      navigate('/receipts', { replace: true });
+    } else {
+      updateReceipt(receiptData).unwrap().then(
+        () => {
+          console.log('succeeded')
+        }
+      ).catch((error) => {
+        console.log('error')
+      });
+    }
+  };
+
+  const handleCancel = async () => {
+    navigate('/receipts', { replace: true });
+  };
 
   const [date, setDate] = useState(dayjs(item?.receiptDatetime));
   const [amount, setAmount] = useState(item?.totalAmount);
@@ -33,8 +74,8 @@ export default function EditReceipt() {
       {item ? (
         // Render content when itemId is available
         <div>
-          <Button onClick={() => { console.log('confirm') }}>Confirm</Button>
-          <Button onClick={() => { console.log('cancel') }}>Cancel</Button>
+          <Button onClick={handleConfirmClick}>Confirm</Button>
+          <Button onClick={handleCancel}>Cancel</Button>
           <div>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <DemoContainer components={['DatePicker', 'DatePicker']}>
@@ -71,7 +112,7 @@ export default function EditReceipt() {
                   src={`https://api.ireceipts.au/Receipt/GetImage/${encodeURIComponent(item.imagePath)}`}
                   alt={item.companyName}
                   loading="lazy"
-                />
+                                />
               </ImageListItem>
             </Card>
           </div>
@@ -80,7 +121,8 @@ export default function EditReceipt() {
         // Render content when itemId is not available
         <div>
           <h4>No receipt item selected.</h4>
-          <Button onClick={console.log('cancel')}>Cancel</Button>
+          <Button onClick={handleCancel}>Cancel</Button>
+
         </div>
       )}
     </Box>
