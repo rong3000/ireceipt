b30dc6e65aca0d03b088fc620002da40f168dc9b
Rong <rong.lin3000@gmail.com>
2023-12-12 11:25:15 +1100

    add signup to redux

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,27 +1,59 @@
-import auth from '../../firebase'
-import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from './firebase'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
 export const login = (email, password) => async (dispatch) => {
   try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
     console.log('from firebase ', userCredential)
 
-    const user = { email: userCredential.user.email, accessToken: userCredential.user.accessToken };
+        const user = { email: userCredential.user.email, accessToken: userCredential.user.accessToken };
 
-    dispatch({
+        dispatch({
       type: 'LOGIN_SUCCESS',
       payload: user,
     });
   } catch (error) {
     // Handle login error
-    console.log(error)
+    dispatch({
+      type: 'LOGIN_FAILED',
+      payload: error.code
+    });
   }
 };
 
 export const logout = () => async (dispatch) => {
   try {
-    dispatch({ type: 'LOGOUT_SUCCESS' });
+        dispatch({ type: 'LOGOUT_SUCCESS' });
   } catch (error) {
-    console.log(error)
+    // Handle logout error
+  }
+};
+
+export const authInit = () => async (dispatch) => {
+  try {
+    dispatch({ type: 'AUTHINIT' });
+  } catch (error) {
+    // Handle logout error
+  }
+};
+
+export const signup = (email, password) => async (dispatch) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    console.log(userCredential.user.accessToken)
+    console.log(userCredential.user.email)
+
+    const user = { email: userCredential.user.email, accessToken: userCredential.user.accessToken };
+
+    dispatch({
+      type: 'SIGNUP_SUCCESS',
+      payload: user,
+    });
+  } catch (error) {
+    // Handle login error
+    dispatch({
+      type: 'SIGNUP_FAILED',
+      payload: error.code
+    });
   }
 };
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,9 +1,9 @@
 const initialState = {
     user: null,
     isLoggedIn: false
-  };
+};
 
-export default function authReducer (state = initialState, action) {
+export default function authReducer(state = initialState, action) {
     switch (action.type) {
         case 'LOGIN_SUCCESS':
             return {
@@ -11,12 +11,34 @@ export default function authReducer (state = initialState, action) {
                 user: action.payload, //action.payload or action.data
                 isLoggedIn: true,
             };
+        case 'LOGIN_FAILED':
+            return {
+                error: action.payload, //action.payload or action.data
+                isLoggedIn: false,
+            };
         case 'LOGOUT_SUCCESS':
             return {
                 ...state,
                 user: null,
                 isLoggedIn: false,
             };
+        case 'AUTHINIT':
+            return {
+                user: null,
+                isLoggedIn: false,
+                error: null
+            };
+            case 'SIGNUP_SUCCESS':
+                return {
+                    ...state,
+                    user: action.payload, //action.payload or action.data
+                    isLoggedIn: true,
+                };
+            case 'SIGNUP_FAILED':
+                return {
+                    error: action.payload,
+                    isLoggedIn: false,
+                };
         default:
             return state;
     }
